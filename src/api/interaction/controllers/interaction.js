5bd2c8ed101a221b8100521468a229e517f88578
gabriel23002 <gabriel23002@gmail.com>
2023-02-25 21:53:25 -0300

    feat: Get quiz info in highlight

--- src/api/interaction/controllers/interaction.js
+++ src/api/interaction/controllers/interaction.js
@@ -9,7 +9,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::interaction.interaction', {
   async count(ctx) {
     var { query } = ctx.request
-    const interactions = await strapi.db.connection.raw('select count(quiz), quiz from interactions group by quiz order by count(quiz) limit 10;')
+    const interactions = await strapi.db.connection.raw("select count(quiz_id) as anshwered, quiz_id, q.title, fl.url from interactions_quiz_links i left join quizzes q on q.id = i.quiz_id left join files_related_morphs f on f.related_id = i.quiz_id and f.related_type = 'api::quiz.quiz' left join files fl on fl.id = f.file_id group by quiz_id, q.id, fl.id order by count(quiz_id) limit 10;")
     return interactions
   }
 });
